1742bd0dd5beee1d886acf462ae863b037718523
hedfi <hedfi.haythem@gmail.com>
2021-10-28 18:47:59 +0100

    graphql start

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-})
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: buildSchema(`
+        type RootQuery {
+            todos: [String!]!
+        }
+        type RootMutation {
+            createTodo(title: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+        rootValue: {
+            todos: () => {
+                return ['Meet George', 'Buy eggs', 'Read a book', 'Organize office'];
+            },
+            createTodo: ({ title }) => {
+                return title;
+            }
+        },
+        graphiql: true
+    })
+);
 
 app.listen(3000);
