7796807c23dd3abae19c897afe7a05e8d540df02
hedfi <hedfi.haythem@gmail.com>
2021-10-28 19:06:39 +0100

    graphql types

--- app.js
+++ app.js
@@ -5,17 +5,30 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const todos = [];
+
 app.use(bodyParser.json());
 
 app.use(
     '/graphql',
     graphqlHTTP({
         schema: buildSchema(`
+        type Todo {
+          title: String!
+          description: String
+          date: String!
+          completed: Boolean!
+        }
+        input TodoInput {
+          title: String!
+          description: String
+        }
+        
         type RootQuery {
-            todos: [String!]!
+            todos: [Todo!]!
         }
         type RootMutation {
-            createTodo(title: String): String
+            createTodo(title: String, description: String): Todo
         }
         schema {
             query: RootQuery
@@ -24,10 +37,17 @@ app.use(
     `),
         rootValue: {
             todos: () => {
-                return ['Meet George', 'Buy eggs', 'Read a book', 'Organize office'];
+                return todos;
             },
-            createTodo: ({ title }) => {
-                return title;
+            createTodo: ({ title, description }) => {
+                const todo = {
+                    title,
+                    description,
+                    date: new Date().toISOString(),
+                    completed: false
+                };
+                todos.push(todo);
+                return todo;
             }
         },
         graphiql: true
